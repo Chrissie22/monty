416b4e5c7366e21f112f7eb10fe6b1912dcec060
Chrissie <christabelojobolo@yahoo.com>
2024-02-04 13:09:50 -0800

    Header file

--- monty.h
+++ monty.h
@@ -70,6 +70,7 @@ void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
+void torl(stack_t **stack, unsigned int line_number);
 
 void free_data(void);
 void free_stack(stack_t **stack);
